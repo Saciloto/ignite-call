07a92675045c39439f46273918726ecbf8eb0c1a
saciloto <mauriciosaciloto@gmail.com>
2023-05-09 16:34:36 -0300

    initial caommit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,27 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+import previewImage from '../../assets/appPreview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário de agendamento"
+          height={400}
+          quality={100} // padrao next é 80
+          priority // Da prioridade para a imagem carregar
+        />
+      </Preview>
+    </Container>
+  )
+}
