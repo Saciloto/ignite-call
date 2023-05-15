7bfe0dd95621aa91c32f30370420f2ef6d0d643d
saciloto <mauriciosaciloto@gmail.com>
2023-05-15 16:32:50 -0300

    Finalizing calendar styles

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,43 @@
+import { Calendar } from '@/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+import { useState } from 'react'
+
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça feira <span>20 de setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,7 @@
+// import { CalendarStep } from './CalendarStep'
+import { CalendarStep } from './CalendarStep'
+// import { ConfirmStep } from './Confirmstep'
+
+export function ScheduleForm() {
+  return <CalendarStep />
+}
